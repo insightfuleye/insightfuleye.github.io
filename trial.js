d5ed2dc31574d578514663558061f6e4d9ba2415
insightfuleye <149656331+insightfuleye@users.noreply.github.com>
2023-11-03 21:24:56 -0700

    Create trial.js

---
+++ trial.js
@@ -0,0 +1,20 @@
+// This code is for v4 of the openai package: npmjs.com/package/openai
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+function GenerateText() {
+const response = await openai.completions.create({
+    model: "text-davinci-003",
+    prompt: "write a story incorporating the following fact: there are more grains of sand on earth than stars in the sky",
+    temperature: 1,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+});
+  document.getElementById('generated_text').innerHTML = response;
+}
+console.log(response)
